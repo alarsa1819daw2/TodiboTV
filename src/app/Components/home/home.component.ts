9f3463a71e375c0a43a615110a040bf50ab8ec7f
alarsa1819daw2 <alarsa1819daw2@gmail.com>
2019-01-15 19:50:21 +0100

    Pipe y home atualizado (Presenta las peliculas que haya en el Model)

--- src/app/Components/home/home.component.ts
+++ src/app/Components/home/home.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, HostListener } from '@angular/core';
+import { PELICULAS } from 'src/app/Models/pelicula';
+
+
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+export class HomeComponent {
+  
+  peliculas = PELICULAS;
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Components/home/home.component';
 import { BuscadorComponent } from './Components/buscador/buscador.component';
+import { FilterPipe } from './filter.pipe';
+import { PeliculaComponent } from './Components/pelicula/pelicula.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    BuscadorComponent
+    BuscadorComponent,
+    FilterPipe,
+    PeliculaComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
