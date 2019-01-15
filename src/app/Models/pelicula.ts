f9064c790cbe7221af7515fed7e317a173e8541f
alarsa1819daw2 <alarsa1819daw2@gmail.com>
2019-01-15 19:48:05 +0100

    Componente y model Pelicula

---
+++ src/app/Models/pelicula.ts
@@ -0,0 +1,12 @@
+export class Pelicula {
+    name: string;
+    description: string;
+    nomDirector: string;
+  }
+  
+  export const PELICULAS = [
+    {name: 'Titanic', description: 'oigdn', nomDirector:'ognue'},
+    {name: 'Rey León', description: 'oigdn', nomDirector:'ognue'},
+    {name: 'Putos', description: 'oigdn', nomDirector:'ognue'}
+  ];
+  
