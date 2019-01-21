1ad8a00f7e1c69d6315e389ef4b859bf5c0fb07c
alarsa1819daw2 <alarsa1819daw2@gmail.com>
2019-01-21 22:54:14 +0100

    Favoritos implementado

--- src/app/Components/favoritos/favoritos.component.ts
+++ src/app/Components/favoritos/favoritos.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { PELICULAS } from 'src/app/Models/pelicula';
 
 
@@ -7,7 +7,34 @@ import { PELICULAS } from 'src/app/Models/pelicula';
   templateUrl: './favoritos.component.html',
   styleUrls: ['./favoritos.component.css']
 })
-export class FavoritosComponent {
+export class FavoritosComponent implements OnInit{
 
   peliculas = PELICULAS;
+
+  proba: string;
+  pelisFav: string[];
+  
+  constructor() {
+    this.pelisFav = [];
+  }
+
+  ngOnInit(){
+    for(var i=0;i<50;i++) {
+      this.proba = localStorage.getItem(i.toString());
+      if (!this.proba){
+        console.log("if" + this.proba);
+      }else{
+        console.log("elseee" + this.proba);
+        this.pelisFav.push(this.proba);
+      } 
+    }     
+  }
+
+  clickBorrarFav() {
+    for(var i=0;i<50;i++) {
+      localStorage.removeItem(i.toString());
+    }
+    this.pelisFav = [];
+  }
+
 }
